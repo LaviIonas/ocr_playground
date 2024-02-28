d834d4f0cdf821f297bd32fe947a1c19dc460d5a
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2024-02-28 17:30:09 -0500

    pytess basics done

--- src/2_0_pytesseract_basics.py
+++ src/2_0_pytesseract_basics.py
@@ -2,4 +2,11 @@ import pytesseract
 from PIL import Image
 
 img_file = "../data/page_01.jpg"
+no_noise = "temp/no_noise.jpg"
+
+img = Image.open(no_noise)
+
+ocr_result = pytesseract.image_to_string(no_noise)
+
+print(ocr_result)
 
