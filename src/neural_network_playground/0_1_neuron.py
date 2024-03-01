4efafcda114dc013a5159f6d5cf1b804834919a0
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2024-03-01 12:36:12 -0500

    convert neuron to oop

--- src/neural_network_playground/0_1_neuron.py
+++ src/neural_network_playground/0_1_neuron.py
@@ -1,13 +1,16 @@
 import numpy as np
 
-inputs = [1, 2, 3, 2.5] 
+inputs = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
 weights = [[0.2, 0.8, -0.5, 1.0], [0.5, -0.91, 0.26, -0.5], [-0.26, -0.27, 0.17, 0.87]]
 biases = [2, 3, 0.5]
 
-output = np.dot(weights, inputs) + biases
-print(output)
+weights1 = [[0.1, -0.14, 0.5], [-0.5, 0.12, -.033], [-0.44, 0.73, -0.13]]
+biases1 = [-1, 2, -0.5]
 
+layer1_output = np.dot(inputs, np.array(weights).T) + biases
+layer2_output = np.dot(layer1_output, np.array(weights1).T) + biases1
 
+print(layer2_output)
 
 # layer_outputs = []
 # for neuron_weights, neuron_bias in zip(weights, biases):
