695e281e35c6a105948b08424bde81a1098574b9
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2024-03-02 17:03:52 -0500

    Update 2_1_neural_net_ocr_class.py

---
+++ src/neural_network_playground/2_1_neural_net_ocr_class.py
@@ -0,0 +1,62 @@
+import numpy as np
+import math
+from matplotlib import pyplot as plt
+
+DATA_DIR = '../../MNIST_DATA'
+TEST_DATA_FILENAME = DATA_DIR + '/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte'
+TEST_LABELS_FILENAME = DATA_DIR + '/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte'
+TRAIN_DATA_FILENAME = DATA_DIR + '/train-images-idx3-ubyte/train-images-idx3-ubyte'
+TRAIN_LABELS_FILENAME = DATA_DIR + '/train-labels-idx1-ubyte/train-labels-idx1-ubyte'
+
+# convert bytes to ints
+def bytes_to_int(byte_data):
+    return int.from_bytes(byte_data, 'big')
+
+def read_images(filename, n_max_images=None):
+    images = []
+    with open(filename, 'rb') as f:
+        _ = f.read(4) # magic number
+        n_images = bytes_to_int(f.read(4))
+
+        if n_max_images:
+            n_images = min(n_images, n_max_images) #prevent silly mistakes
+        
+        n_rows = bytes_to_int(f.read(4))
+        n_cols = bytes_to_int(f.read(4))
+
+        for image_idx in range(n_images):
+            image_data = np.frombuffer(f.read(n_rows*n_cols), dtype=np.uint8)
+            image = image_data.reshape((n_rows, n_cols))
+            images.append(image.T.flatten())
+    return images
+
+def read_labels(filename, n_max_labels=None):
+    labels = []
+    with open(filename, 'rb') as f:
+        _ = f.read(4) # magic number
+        n_labels = bytes_to_int(f.read(4))
+        
+        if n_max_labels:
+            n_labels = min(n_labels, n_max_labels) #avoid silly mistake
+
+        label_data = np.frombuffer(f.read(n_labels), dtype=np.uint8)
+        labels = label_data.tolist()
+    return labels
+
+def main():
+    n_max = 1
+    X_train = read_images(TRAIN_DATA_FILENAME, n_max) 
+    y_train = read_labels(TRAIN_LABELS_FILENAME, n_max) 
+    X_test = read_images(TEST_DATA_FILENAME, n_max)
+    y_test = read_labels(TEST_LABELS_FILENAME, n_max)
+
+    X_train = X_train.reshape(60000, 784)
+    X_test = X_test.reshape(60000, 784)
+
+    
+    print(X_train.shape)
+    print(X_test.shape)
+    
+    
+if __name__ == '__main__':
+    main()
