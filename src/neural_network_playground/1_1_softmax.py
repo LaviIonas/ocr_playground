7d5d20c9cb11d0a54c0c656d021a4569f2d476fc
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2024-03-02 12:33:51 -0500

    loss function

--- src/neural_network_playground/1_1_softmax.py
+++ src/neural_network_playground/1_1_softmax.py
@@ -21,6 +21,24 @@ class Activation_Softmax:
         prob = exp_values / np.sum(exp_values, axis=1, keepdims=True)
         self.output = prob
 
+class Loss:
+    def calculate(self, output, y):
+        sample_loses = self.forward(output, y)
+        data_loss = np.mean(sample_loses)
+        return data_loss
+    
+class Loss_CategoricalCrossEntropy(Loss):
+    def forward(self, y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1-1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped*y_true, axis=1)
+
+        return -np.log(correct_confidences)
+
 X, y = spiral_data(samples=100, classes=3)
 
 dense1 = Layer_Dense(2,3)
@@ -36,3 +54,8 @@ dense2.forward(act1.output)
 act2.forward(dense2.output)
 
 print(act2.output[:5])
+
+loss_function = Loss_CategoricalCrossEntropy()
+loss = loss_function.calculate(act2.output, y)
+
+print("Loss:", loss)
