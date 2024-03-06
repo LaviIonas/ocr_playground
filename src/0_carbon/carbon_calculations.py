0f913403dfd139d1ad58163bafd6a985d9ff8fa4
Lavi Ionas <46603873+LaviIonas@users.noreply.github.com>
2024-03-06 15:32:22 -0500

    Update carbon_calculations.py

--- src/0_carbon/carbon_calculations.py
+++ src/0_carbon/carbon_calculations.py
@@ -7,42 +7,57 @@ import netCDF4 as cdf
 FILE_DIR = ""
 FILE_NAME = "AllWood_2006-2022.csv"
 
-def first_order_decay_sw(k=k , inflow=inflow_t0, c_t=c_t0): 
-    return np.exp(-1*k)*c_i + ((1-np.exp(-1*k))/k)*inflow
-
-def get_data(file_name):
-    # with xr.open_dataset(file_name) as ds:
-    #     paris_masks = ds
-
+# Function that reads CSV file and outputs an array
+def load_data(file_name):
+    # Read CSV
     df = pd.read_csv(file_name)
+    # Process data
+    data = process_data(df)
 
+    return data
+
+# Function that processes the dataframe 
+def process_data(df):
+    # Process data
     df.loc[(df['Element']=='Export Quantity'), 'Value']*= -1
+    # Organize data
+    return df[['Item', 'Area', 'Year', 'Value']].groupby(['Item', 'Area', 'Year']).sum().reset_index()
 
-    nt_df = df[['Item', 'Area', 'Year', 'Value']].groupby(['Item', 'Area', 'Year']).sum().reset_index()
+# Function that gets the data slice for a specific country
+def get_country_data(data, area, item):
+    country_data = data[data["Area"] == area]
+    return country_data
 
-    return nt_df
+# 
+def define_inflow_carbon(data, area, item, parameter_dict):
+    items = country_data[country_data['Item'] == item]
 
-def generate_carbon_fraction(data, area, item):
-    carbon_fraction = data[(data['Area'] == area)& (data['Item'] == item)]
-    carbon_fraction['Inflow_c'] = carbon_fraction['Value']/410*0.5
+    multiplier = get_multiplier(item, parameter_dict)
+    for value in items['Value']:
+        items['InflowC'] = value / 410.0 * multiplier
 
-    return nt_cf['Inflow_c']
+    print(items)
 
-def calcualte_decay_per_year(data, half_life, area, item):
-    k = np.log(2)/half_life 
-    inflow_t0 = generate_carbon_fraction(data, area, item).mean() 
-    c_t0 = inflow_t0 / k 
+    # carbon_fraction = data[(data['Area'] == area) & (data['Item'] == item)]
+    # carbon_fraction['Inflow_c'] = carbon_fraction['Value']/410*0.5
 
-    first_order_decay = first_order_decay_sw(k=k, inflow=inflow_t0, c_t=c_t0)
+    # return carbon_fraction['Inflow_c']
 
-    return first_order_decay
+# def calcualte_decay_per_year(data, half_life, area, item):
+#     k = np.log(2)/half_life 
+#     inflow_t0 = generate_carbon_fraction(data, area, item).mean() 
+#     c_t0 = inflow_t0 / k 
+
+#     first_order_decay = first_order_decay_sw(k=k, inflow=inflow_t0, c_t=c_t0)
+
+#     return first_order_decay
 
 
-def generate_decay_array(data, half_life, area, item):
-    decay_array = []
+# def generate_decay_array(data, half_life, area, item):
+#     decay_array = []
 
-    inflow_t0 = generate_carbon_fraction(data, area, item).mean() 
-    print(inflow_t0)
+#     inflow_t0 = generate_carbon_fraction(data, area, item).mean() 
+#     print(inflow_t0)
     # c_t0 = inflow_t0 / k     
 
     # c_t = c_t0
@@ -55,13 +70,36 @@ def generate_decay_array(data, half_life, area, item):
     #     print(c_t)
 
 
+# def first_order_decay_sw(k , inflow_t0, c_t0): 
+#     return np.exp(-1*k)*c_i + ((1-np.exp(-1*k))/k)*inflow
+
+def get_multiplier(item, parameter_dict):
+    parameters = parameter_dict.get(item, {}).values()
+    multiplier = 1
+    for value in parameters:
+        multiplier *= value
+
+    return multiplier
+
 def main():
+
+    parameter_dict = {
+        'Sawnwood': {
+            'c_fraction': 0.5
+        } 
+    }
+
     path = FILE_DIR + FILE_NAME
-    nt_df = get_data(path)
+    data = load_data(path)
 
-    generate_decay_array(nt_df, 35, 'Finland', "Sawnwood")
+    c = get_country_data()
+    define_inflow_carbon(data, 'Finland', "Sawnwood", parameter_dict)
 
-    print(nt_df)
+    # print(nt_df[:5])
+
+    # generate_decay_array(nt_df, 35, 'Finland', "Sawnwood")
+
+    # print(nt_df)
 
 if __name__ == '__main__':
     main()
